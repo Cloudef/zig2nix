ae79308c4f876ee6bfeae78b74b0b3ad6017f622
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2024-06-13 09:24:59 +0900

    test: remove .zig-cache as well

--- src/test.nix
+++ src/test.nix
@@ -77,6 +77,7 @@ with lib;
       rm -f templates/"$var"/result
       rm -rf templates/"$var"/zig-out
       rm -rf templates/"$var"/zig-cache
+      rm -rf templates/"$var"/.zig-cache
     done
     '';
 
